3bbde343c2589553193719515e90e796d14dd0d1
liyanke <liyanke@baidu.com>
2017-12-17 18:14:39 +0800

    Initial Commit

---
+++ WallPaperForUnsplash/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WallPaperForUnsplash
+//
+//  Created by great Lock on 2017/12/17.
+//  Copyright © 2017年 great Lock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WallPaperForUnsplash/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WallPaperForUnsplash
+//
+//  Created by great Lock on 2017/12/17.
+//  Copyright © 2017年 great Lock. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
